56ad7a7bd47785c035549ff7f88a6915b01a52c4
Zach Estrella <zachgabriel27@gmail.com>
2026-04-20 18:43:46 +0800

    Fix card responsiveness

--- components/reusable/Card.tsx
+++ components/reusable/Card.tsx
@@ -5,7 +5,7 @@ type CardProps = {
 
 export const Card = (props:CardProps) => {
     return (
-        <div className={`${props.class} bg-white text-center shadow-lg pb-4 rounded-lg `}>
+        <div className={`${props.class} bg-white text-center shadow-lg pb-4 rounded-lg lg:w-full w-3/4 mx-auto`}>
             {props.children}
         </div>
     )
